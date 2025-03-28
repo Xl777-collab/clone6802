e28a687233c40bc95af3f5e2676556be3587cb4a
Xl777-collab <lihengxl777@gmail.com>
2025-03-28 12:26:15 +0000

    1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 import datetime
 import google.generativeai as genai
 import os
+import wikipedia
 
 api = "AIzaSyANexXf85IyyaPmFLtKmndYnvlSM48_NXI"
 model = genai.GenerativeModel("gemini-1.5-flash")
@@ -35,6 +36,10 @@ def main():
 def foodexp():
     return(render_template("foodexp.html"))
 
+@app.route("/foodexp1",methods=["POST","GET"])
+def foodexp1():
+    return(render_template("foodexp1.html"))
+
 @app.route("/foodexp_pred",methods=["POST","GET"])
 def foodexp_pred():
     q = float(request.form.get("q"))
@@ -60,6 +65,12 @@ def FAQ():
 def FAQ1():
     r = model.generate_content("Factors for Profit")
     return(render_template("FAQ1.html",r=r.candidates[0].content.parts[0]))
+
+@app.route("/FAQinput",methods=["POST","GET"])
+def FAQ1input():
+    q = request.form.get("q")
+    r = wikipedia.summary(q)
+    return(render_template("FAQinput.html",r=r))
 
 @app.route("/userLog",methods=["POST","GET"])
 def userLog():
